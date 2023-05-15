64f172e4d53e9a36cd134bae64a1544e3573c67a
SolForte <mateusbrandao1234@gmail.com>
2023-05-15 13:33:58 -0300

    feat: create rentals

--- src/controllers/rentals.controllers.js
+++ src/controllers/rentals.controllers.js
@@ -10,3 +10,59 @@ export async function getRentals(_req, res) {
     return;
   }
 }
+
+export async function postRentals(req, res) {
+  const { customerId, gameId, daysRented } = req.body;
+  try {
+    const customerQuery = await db.query(
+      `SELECT * FROM customers WHERE id = $1`,
+      [customerId]
+    );
+    const gameQuery = await db.query(`SELECT * FROM games WHERE id=$1`, [
+      gameId,
+    ]);
+    const rentalsQuery = await db.query(
+      `SELECT COUNT(*) FROM rentals WHERE "gameId"=$1 AND "returnDate" IS NULL`,
+      [gameId]
+    );
+
+    const { pricePerDay, stockTotal } = gameQuery.rows[0];
+
+    if (
+      gameQuery.rowCount === 0 ||
+      customerQuery.rowCount === 0 ||
+      stockTotal - rentalsQuery.rows[0].count
+    ) {
+      res.sendStatus(400);
+      return;
+    }
+
+    await db.query(
+      `INSERT INTO rentals (
+            "customerId",
+            "gameId",
+            "rentDate",
+            "daysRented",
+            "returnDate",
+            "originalPrice",
+            "delayFee"
+            )
+            VALUES ($1, $2, $3, $4, $5, $6, $7)`,
+      [
+        customerId,
+        gameId,
+        dayjs().format("YYYY-MM-DD"),
+        daysRented,
+        null,
+        pricePerDay * daysRented,
+        null,
+      ]
+    );
+
+    res.sendStatus(201);
+    return;
+  } catch (error) {
+    res.status(500).send(error.message);
+    return;
+  }
+}
--- src/routers/rentals.router.js
+++ src/routers/rentals.router.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getRentals } from "../controllers/rentals.controllers.js";
+import { getRentals, postRentals } from "../controllers/rentals.controllers.js";
+import rentalSchema from "../schemas/rentals.schema.js";
+import validateSchema from "../middlewares/validateSchema.middleware.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.get("/rentals", getRentals);
+rentalsRouter.post("/rentals", validateSchema(rentalSchema), postRentals);
 
 export default rentalsRouter;
