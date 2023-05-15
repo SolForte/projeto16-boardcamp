f703ecc137063c73f348dc018742e5769d33cb84
SolForte <mateusbrandao1234@gmail.com>
2023-05-15 17:05:05 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use(routers);
 
-app.listen(process.env.PORT, () =>
-  console.log(`Running server on port ${process.env.PORT}.`)
-);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
