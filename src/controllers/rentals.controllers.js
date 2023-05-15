3f931b0c39114e430682b5cda9bb43f768aebbaf
SolForte <mateusbrandao1234@gmail.com>
2023-05-15 04:04:01 -0300

    feat: get rentals

---
+++ src/controllers/rentals.controllers.js
@@ -0,0 +1,10 @@
+import { db } from "../database/database.js";
+
+export async function getRentals(_req, res) {
+  try {
+    const rentals = await db.query(`SELECT * FROM rentals`);
+    res.status(200).send(rentals.rows);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,9 +1,11 @@
-import {Router} from "express";
+import { Router } from "express";
 import gamesRouter from "./games.router.js";
 import customersRouter from "./customers.router.js";
+import rentalsRouter from "./rentals.router.js";
 
 const routers = Router();
 routers.use(gamesRouter);
 routers.use(customersRouter);
+routers.use(rentalsRouter);
 
 export default routers;
